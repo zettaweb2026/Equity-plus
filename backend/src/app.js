97d4f6e7b87d94ba3ca021db0013eb03e6650282
Arunmaity2003 <arunmaity0610@gmail.com>
2026-07-02 08:14:36 +0530

    git ignore and packages

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,12 @@ app.use(express.json());
 app.use(logger);
 app.use('/api', routes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the backend API!');
+});
+
 app.use((req, res) => {
-  res.status(404).json({ error: 'Not found' });
+  res.status(404).json({ error: 'Not Found' });
 });
 
 module.exports = app;
