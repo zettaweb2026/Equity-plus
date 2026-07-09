f7a9042593f805653c3657bc8f090598c338f5a6
Samir linux <samirbrahma2003@gmail.com>
2026-07-09 18:19:33 +0530

    home ui chage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,12 +11,7 @@ const Home = () => {
         <Herohome />
       </section>
 
-      <section className="px-4 py-5 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
-        <h1 className="my-6 text-center text-2xl font-semibold text-sky-700 sm:text-3xl lg:text-4xl">
-          Today's Market Review
-        </h1>
-        <Sechome />
-      </section>
+
 
       <section className="px-4 py-5 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
         <h1 className="my-6 text-center text-2xl font-semibold text-sky-700 sm:text-3xl lg:text-4xl">
